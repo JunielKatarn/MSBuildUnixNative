06482e9feef11b74cb08c84e2143cb271d70b3d9
Julio C. Rocha <julio@rochsquadron.net>
2016-07-27 02:08:07 -0700

    Make text files use LF endings by default.

--- Source/Tasks/CleanTask.cs
+++ Source/Tasks/CleanTask.cs
@@ -1,4 +1,4 @@
-﻿#region Using directives
+#region Using directives
 
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
@@ -11,158 +11,161 @@ using System.IO;
 
 namespace LLVM.Build.Tasks
 {
-    /// <summary>
-    /// This is an MSBuild Task that allows deleting files that match the 
-    /// specified list of extensions inside the list of specified directories.
-    /// </summary>
-    public class CleanTask : Task
-    {
-        #region Private members
+	/// <summary>
+	/// This is an MSBuild Task that allows deleting files that match the 
+	/// specified list of extensions inside the list of specified directories.
+	/// </summary>
+	public class CleanTask : Task
+	{
+		#region Private members
 
-        private const string DefaultExtension = "*";
-        private readonly char[] argumentSeparators = { ';' };
+		private const string DefaultExtension = "*";
+		private readonly char[] argumentSeparators = { ';' };
 
-        private string _directories = String.Empty;
-        private string _extensions  = String.Empty;
+		private string _directories = String.Empty;
+		private string _extensions  = String.Empty;
 
-        private List<string> directoriesList = new List<string>();
-        private List<string> extensionsList  = new List<string>();
+		private List<string> directoriesList = new List<string>();
+		private List<string> extensionsList  = new List<string>();
 
-        private bool usingDefaultExtension = false;
+		private bool usingDefaultExtension = false;
 
-        #endregion
-        
-        #region Public properties
+		#endregion
+		
+		#region Public properties
 
-        /// <summary>
-        /// Public property to indicate the list of directories where we want to find files that need to be cleaned.
-        /// This is a required property, so it cannot be empty.
-        /// Multiple directories must be separated by semicolon ";".
-        /// </summary>
-        [Required]
-        public string Directories
-        {
-            get { return _directories; }
-            set
-            {
-                _directories = value;
-                directoriesList.Clear();
-                directoriesList.AddRange(_directories.Split(argumentSeparators, StringSplitOptions.RemoveEmptyEntries));
+		/// <summary>
+		/// Public property to indicate the list of directories where we want to find files that need to be cleaned.
+		/// This is a required property, so it cannot be empty.
+		/// Multiple directories must be separated by semicolon ";".
+		/// </summary>
+		[Required]
+		public string Directories
+		{
+			get { return _directories; }
+			set
+			{
+				_directories = value;
+				directoriesList.Clear();
+				directoriesList.AddRange(_directories.Split(argumentSeparators, StringSplitOptions.RemoveEmptyEntries));
 
-                if (directoriesList.Count <= 0)
-                {
-                    throw new ArgumentNullException("DirectoriesToClean must not be empty.");
-                }
-            }
-        }
+				if (directoriesList.Count <= 0)
+				{
+					throw new ArgumentNullException("DirectoriesToClean must not be empty.");
+				}
+			}
+		}
 
-        /// <summary>
-        /// Public property to indicate the list of extensions that need to be cleaned.
-        /// This is a required property, so it cannot be empty.
-        /// If it is desired to delete all the files, use "*".
-        /// Multiple extensions must be separated by semicolon ";".
-        /// </summary>
-        [Required]
-        public string Extensions
-        {
-            get { return _extensions; }
-            set
-            {
-                _extensions = value;
-                extensionsList.Clear();
-                extensionsList.AddRange(_extensions.Split(argumentSeparators, StringSplitOptions.RemoveEmptyEntries));
+		/// <summary>
+		/// Public property to indicate the list of extensions that need to be cleaned.
+		/// This is a required property, so it cannot be empty.
+		/// If it is desired to delete all the files, use "*".
+		/// Multiple extensions must be separated by semicolon ";".
+		/// </summary>
+		[Required]
+		public string Extensions
+		{
+			get { return _extensions; }
+			set
+			{
+				_extensions = value;
+				extensionsList.Clear();
+				extensionsList.AddRange(_extensions.Split(argumentSeparators, StringSplitOptions.RemoveEmptyEntries));
 
-                if (extensionsList.Count <= 0)
-                {
-                    throw new ArgumentNullException("ExtensionsToClean must not be empty.");
-                }
+				if (extensionsList.Count <= 0)
+				{
+					throw new ArgumentNullException("ExtensionsToClean must not be empty.");
+				}
 
-                usingDefaultExtension = extensionsList.Contains(DefaultExtension);
-            }
-        }
+				usingDefaultExtension = extensionsList.Contains(DefaultExtension);
+			}
+		}
 
-        #endregion
+		#endregion
 
-        #region Private methods
+		#region Private methods
 
-        /// <summary>
-        /// Deletes all the files that match the specified extension inside the desired directory.
-        /// </summary>
-        /// <param name="directory">The directory where the files will be searched and deleted.</param>
-        /// <param name="extension">The extension of the files that need to be deleted, which needs to be provided in the format "*.ext". If all files are desired, use DefaultExtension="*".</param>
-        /// <returns>True if the task was successful, false otherwise.</returns>
-        private bool DeleteFilesInDirectoryByExtension(string directory, string extension)
-        {
-            try
-            {
-                string[] filePaths = Directory.GetFiles(directory, extension, SearchOption.TopDirectoryOnly);
+		/// <summary>
+		/// Deletes all the files that match the specified extension inside the desired directory.
+		/// </summary>
+		/// <param name="directory">The directory where the files will be searched and deleted.</param>
+		/// <param name="extension">
+		///	 The extension of the files that need to be deleted, which needs to be provided in the format
+		///	 "*.ext". If all files are desired, use DefaultExtension="*".
+		/// </param>
+		/// <returns>True if the task was successful, false otherwise.</returns>
+		private bool DeleteFilesInDirectoryByExtension(string directory, string extension)
+		{
+			try
+			{
+				string[] filePaths = Directory.GetFiles(directory, extension, SearchOption.TopDirectoryOnly);
 
-                foreach (string filePath in filePaths)
-                {
-                    string trimmedFilePath = filePath.Trim();
-                    Log.LogMessage("Cleaning file: {0}", trimmedFilePath);
-                    File.Delete(trimmedFilePath);
-                }
-                
-            }
-            catch (Exception e)
-            {
-                Log.LogErrorFromException(e);
-                return false;
-            }
+				foreach (string filePath in filePaths)
+				{
+					string trimmedFilePath = filePath.Trim();
+					Log.LogMessage("Cleaning file: {0}", trimmedFilePath);
+					File.Delete(trimmedFilePath);
+				}
+				
+			}
+			catch (Exception e)
+			{
+				Log.LogErrorFromException(e);
+				return false;
+			}
 
-            return true;
-        }
+			return true;
+		}
 
-        #endregion
+		#endregion
 
-        #region Public methods
+		#region Public methods
 
-        /// <summary>
-        /// The public execution method required by MSBuild.
-        /// It will delete all the files that match the selected extensions
-        /// located inside the specified directories.
-        /// </summary>
-        /// <returns>True if the task was successful, false otherwise.</returns>
-        public override bool Execute()
-        {
-            foreach (string directory in directoriesList)
-            {
-                if (!Directory.Exists(directory))
-                {
-                    Log.LogWarning("Directory does not exist: " + directory);
-                    continue;
-                }
+		/// <summary>
+		/// The public execution method required by MSBuild.
+		/// It will delete all the files that match the selected extensions
+		/// located inside the specified directories.
+		/// </summary>
+		/// <returns>True if the task was successful, false otherwise.</returns>
+		public override bool Execute()
+		{
+			foreach (string directory in directoriesList)
+			{
+				if (!Directory.Exists(directory))
+				{
+					Log.LogWarning("Directory does not exist: " + directory);
+					continue;
+				}
 
-                if (usingDefaultExtension)
-                {
-                    Log.LogWarning("You selected * as extension, which will delete all the files in the selected directories.");
-                    if (!DeleteFilesInDirectoryByExtension(directory, DefaultExtension))
-                    {
-                        return false;
-                    }
-                }
+				if (usingDefaultExtension)
+				{
+					Log.LogWarning("You selected * as extension, which will delete all the files in the selected directories.");
+					if (!DeleteFilesInDirectoryByExtension(directory, DefaultExtension))
+					{
+						return false;
+					}
+				}
 
-                else
-                {
-                    foreach (string extension in extensionsList)
-                    {
-                        string completeExtension = extension;
-                        if(!extension.StartsWith("*."))
-                        {
-                            completeExtension = "*." + extension;
-                        }
-                        if (!DeleteFilesInDirectoryByExtension(directory, completeExtension))
-                        {
-                            return false;
-                        }
-                    }
-                }
-            }
+				else
+				{
+					foreach (string extension in extensionsList)
+					{
+						string completeExtension = extension;
+						if(!extension.StartsWith("*."))
+						{
+							completeExtension = "*." + extension;
+						}
+						if (!DeleteFilesInDirectoryByExtension(directory, completeExtension))
+						{
+							return false;
+						}
+					}
+				}
+			}
 
-            return true;
-        }
+			return true;
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
