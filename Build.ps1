ed0c8158bde804d24cede81cbb7de0f09e452dfe
Julio C. Rocha <julio.rocha@microsoft.com>
2016-12-09 01:11:45 -0800

    Make build paths cross-platform.

--- Build.ps1
+++ Build.ps1
@@ -1,13 +1,13 @@
 $repositoryRoot = Get-Location
 
 # Deploy Dummy Task library.
-Set-Location $repositoryRoot/Source/Samples/DummyTask
+Set-Location ("$repositoryRoot/Source/Samples/DummyTask" -replace '/', [IO.Path]::DirectorySeparatorChar)
 dotnet restore
-dotnet publish -o $repositoryRoot/Target/DotNet/Debug/DummyTask
+dotnet publish -o ("$repositoryRoot/Target/DotNet/Debug/DummyTask" -replace '/', [IO.Path]::DirectorySeparatorChar)
 
 # Deploy LLVM Task library.
-Set-Location $repositoryRoot/Source/Tasks
+Set-Location ("$repositoryRoot/Source/Tasks" -replace '/', [IO.Path]::DirectorySeparatorChar)
 dotnet restore
-dotnet publish -o $repositoryRoot/Target/DotNet/Debug/Tasks
+dotnet publish -o ("$repositoryRoot/Target/DotNet/Debug/Tasks" -replace '/', [IO.Path]::DirectorySeparatorChar)
 
 Set-Location $repositoryRoot
