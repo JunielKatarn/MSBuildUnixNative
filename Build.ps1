8fb60a3deeab3dc05af6cda40af19f5b57c904c3
Julio C. Rocha <julio.rocha@microsoft.com>
2016-12-16 04:52:22 -0800

    Made Tasks project a .NET Core library

--- Build.ps1
+++ Build.ps1
@@ -1,13 +1,11 @@
 $repositoryRoot = Get-Location
 
 # Deploy Dummy Task library.
-Set-Location ("$repositoryRoot/Source/Samples/DummyTask" -replace '/', [IO.Path]::DirectorySeparatorChar)
-dotnet restore
-dotnet publish -o ("$repositoryRoot/Target/DotNet/Debug/DummyTask" -replace '/', [IO.Path]::DirectorySeparatorChar)
+##Set-Location ("$repositoryRoot/Source/Samples/DummyTask" -replace '/', [IO.Path]::DirectorySeparatorChar)
+##dotnet restore
+##dotnet publish -o ("$repositoryRoot/Target/DotNet/Debug/DummyTask" -replace '/', [IO.Path]::DirectorySeparatorChar)
 
+$project = ('Source/Tasks' -replace '/', [IO.Path]::DirectorySeparatorChar)
 # Deploy LLVM Task library.
-Set-Location ("$repositoryRoot/Source/Tasks" -replace '/', [IO.Path]::DirectorySeparatorChar)
-dotnet restore
-dotnet publish -o ("$repositoryRoot/Target/DotNet/Debug/Tasks" -replace '/', [IO.Path]::DirectorySeparatorChar)
-
-Set-Location $repositoryRoot
+dotnet restore $project
+dotnet build $project -f netstandard1.3 -o ('Target/DotNet/Debug/Tasks' -replace '/', [IO.Path]::DirectorySeparatorChar)
