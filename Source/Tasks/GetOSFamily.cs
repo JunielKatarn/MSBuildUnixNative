ad55aec6394c2b3e2e3fa5b7f1f1278712ec38f0
Julio C. Rocha <julio.rocha@microsoft.com>
2017-03-21 19:06:42 -0700

    Created prototype task to distinguish OS at runtime.

---
+++ Source/Tasks/GetOSFamily.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+using Microsoft.Build.Utilities;
+
+namespace LLVM.Build.Tasks
+{
+	public class GetOSFamily : Task
+	{
+
+		public override bool Execute()
+		{
+			OSPlatform p;
+			if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+				p = OSPlatform.Windows;
+			else if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+				p = OSPlatform.Linux;
+			else if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+				p = OSPlatform.OSX;
+
+			Log.LogWarning(p.ToString());
+			Log.LogWarning($"W: {OSPlatform.Windows.ToString()}, L: {OSPlatform.Linux.ToString()}, M: {OSPlatform.OSX.ToString()}");
+
+			return true;
+		}
+	}
+}
